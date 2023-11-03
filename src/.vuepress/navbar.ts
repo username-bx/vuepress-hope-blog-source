89afd75ac30df6758584e136f9000f182121bdc4
bx <bx@gmail.com>
2023-11-03 10:37:00 +0800

    删除 narbar (导航栏中) 初始化生成的 banana 文章

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -17,14 +17,6 @@ export default navbar([
           { text: "vuepress1.0 记录", icon: "pen-to-square", link: "3" },
         ],
       },
-      {
-        text: "香蕉",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
-          { text: "香蕉 1", icon: "pen-to-square", link: "1" },
-        ],
-      },
       { text: "樱桃", icon: "pen-to-square", link: "cherry" },
       { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
       "tomato",
