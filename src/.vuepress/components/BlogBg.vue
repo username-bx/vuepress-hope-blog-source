991d651e3b6eb91c0851d6e5568eb37efb671102
benjaminAltman <benjamin@altman.com>
2024-08-30 17:50:08 +0900

    set backgroud color

--- src/.vuepress/components/BlogBg.vue
+++ src/.vuepress/components/BlogBg.vue
@@ -75,12 +75,12 @@ onMounted(() => {
   top: 0;
   width: 100vw;
   height: 100vh;
-  background-color: #e493d0;
-  background-image: radial-gradient(closest-side, rgba(235, 105, 78, 1), rgba(235, 105, 78, 0)),
-    radial-gradient(closest-side, rgba(243, 11, 164, 1), rgba(243, 11, 164, 0)),
-    radial-gradient(closest-side, rgba(254, 234, 131, 1), rgba(254, 234, 131, 0)),
-    radial-gradient(closest-side, rgba(170, 142, 245, 1), rgba(170, 142, 245, 0)),
-    radial-gradient(closest-side, rgba(248, 192, 147, 1), rgba(248, 192, 147, 0));
+  background-color: #f8faf8;
+  // background-image: radial-gradient(closest-side, rgba(235, 105, 78, 1), rgba(235, 105, 78, 0)),
+  //   radial-gradient(closest-side, rgba(243, 11, 164, 1), rgba(243, 11, 164, 0)),
+  //   radial-gradient(closest-side, rgba(254, 234, 131, 1), rgba(254, 234, 131, 0)),
+  //   radial-gradient(closest-side, rgba(170, 142, 245, 1), rgba(170, 142, 245, 0)),
+  //   radial-gradient(closest-side, rgba(248, 192, 147, 1), rgba(248, 192, 147, 0));
 
   background-size: 130vmax 130vmax, 80vmax 80vmax, 90vmax 90vmax, 110vmax 110vmax, 90vmax 90vmax;
   background-position: -80vmax -80vmax, 60vmax -30vmax, 10vmax 10vmax, -30vmax -10vmax, 50vmax 50vmax;
